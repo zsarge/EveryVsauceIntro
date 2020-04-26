c5edd01163ee0d67de2ee5032c3333d1057c1704
zsarge <mcseamusc@gmail.com>
2020-04-26 10:38:10 -0400

    Add stream mixing
    Now, after getting the timestamps from the video,
    scrape.py selects the video and audio streams with
    youtube-dl and downloads/mixes them with FFMPEG.
    
     Add stream mixing
    
     Now, after getting the timestamps from the video,
     scrape.py selects the video and audio streams with
     youtube-dl and downloads/mixes them with FFMPEG.
    
     Future plans include to standardize clip durations,
     to combine clips into a collage, and to allign
     the intro clips better. (The transcript timestamps
     were not as accurate as I had hoped)

--- scrape.py
+++ scrape.py
@@ -1,20 +1,15 @@
-"""
- Idea: Take three passes
- 1: Load the youtube URL
-    Look through it, and find the transcript
-    Find the intro text
-    Load that timestamp
- 2: Download the youtube video
- 3: On completion, cut the youtube video to size.
-  {repeat}
-
-"""
-
 import os.path
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_dl import YoutubeDL
+from ffmpy import FFmpeg
+
+def writeError(fileNumber):
+    errorFile = open('errors.txt', 'a') 
+    errorFile.write(f"Error on file {fileNumber}\n")
+    errorFile.close()
 
 def getFileTitle(index):
-    return(f"Videos/{index}.mp4")
+    return(f"Videos/{index}.mkv")
 
 def getYoutubeURL(videoId):
     return(f"https://www.youtube.com/watch?v={videoId}")
@@ -26,13 +21,48 @@ def getTimestamps(videoId, string):
     for i in transcript:
         if string.lower() in i['text'].lower():
             start = i['start']
-            end = round(i['start']+i['duration'], 3)
-            return [start,end]
+            duration = i['duration']
+            return [start, duration]
 
+def loadVideo(url):
+    with YoutubeDL() as ydl:
+        return ydl.extract_info(url, download=False)
+
+def getVideoStream(info_dict):
+    url = ''
+    maxWidth = 0
+    # Find the widest video only stream
+    for value in info_dict['formats']:
+        if value['acodec'] == 'none':
+            if value['width'] > maxWidth:
+                url = value['url']
+    return url
+
+def getAudioStream(info_dict):
+    # Audio quality doesn't matter as much,
+    # so return the first audio only stream.
+    for value in info_dict['formats']:
+        if value['vcodec'] == 'none':
+            return value['url']
+
+def downloadStreams(vidURL, audURL, startPoint, duration, filename):
+    ff = FFmpeg(
+        inputs={
+            f"{vidURL}" : ['-ss', f"{startPoint}"],
+            f"{audURL}" : ['-ss', f"{startPoint}"]
+        },
+        outputs={
+            f"{filename}": ['-map', '0:v', '-map', '1:a', '-ss', f"{startPoint}", '-t', f"{duration}", "-c:v", "libx264", "-c:a", "aac"]
+        }
+    )
+
+    ff.run()
+
+# ___________________________________________
 # Open list of URLs
 file1 = open('urls.txt', 'r') 
 urlFile = file1.readlines() 
-    # The command used to get the URLS
+    # The command used to get the URLs: (We don't want to have to load this every time)
     # youtube-dl --get-id https://www.youtube.com/playlist?list=PLzyXKIJyRnnNtfLFSaAf7M52cJ64zzQ0A -i >> urls.txt
 
 # Go through every url in urls.txt
@@ -43,15 +73,24 @@ for index, videoId in enumerate(urlFile):
     else:
         try:
             # print(f"\nTime ({videoId.strip()}):",end="\n\t")
-            getTimestamps(videoId, 'vsauce')
+            timestamps = getTimestamps(videoId, 'vsauce')
+
+            info_dict = loadVideo(getYoutubeURL(videoId))
+            vidURL = getVideoStream(info_dict)
+            audURL = getAudioStream(info_dict)
+            
+            downloadStreams(vidURL, audURL, timestamps[0], timestamps[1], getFileTitle(index))
 
         except:
             print(f"\nError occured on video {index}: \"{videoId.strip()}\". Continuing.\n")
+            writeError(index)
+
+file1.close()
 
 # Plans:
 
 # The command to download from youtube:
-# time ffmpeg -ss $start_point -i "$video_url" -ss $start_point -i "$audio_url" -map 0:v -map 1:a -ss $start_point -t $duration -c:v libx264 -c:a aac gog-vs-triv.mkv
+# ffmpeg -ss $start_point -i "$video_url" -ss $start_point -i "$audio_url" -map 0:v -map 1:a -ss $start_point -t $duration -c:v libx264 -c:a aac test.mkv
 
 # Needs:
 #   start_point
